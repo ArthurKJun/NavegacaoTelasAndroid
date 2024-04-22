dd3f0c38b18b52d5b69e85409a0aaa92dc2b2a08
ARTHUR KLUG JUNIOR <arthur.junior@alunos.sc.senac.br>
2024-04-22 19:49:32 -0300

    Criado as telas simples para navegar

--- app/src/main/java/com/senac/navegacaotelas/screens/About.kt
+++ app/src/main/java/com/senac/navegacaotelas/screens/About.kt
@@ -1,2 +1,34 @@
 package com.senac.navegacaotelas.screens
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
+
+@Composable
+fun About(){
+
+    Column (
+        modifier = Modifier
+            .fillMaxSize()
+    ){
+        Text(
+            text = "About",
+            textAlign = TextAlign.Center,
+            fontSize = 24.sp,
+            modifier = Modifier
+                .fillMaxSize()
+        )
+    }
+}
+
+
+@Preview(showBackground = true)
+@Composable
+fun PreviewAbout(){
+    About()
+}
--- app/src/main/java/com/senac/navegacaotelas/screens/Profile.kt
+++ app/src/main/java/com/senac/navegacaotelas/screens/Profile.kt
@@ -1,1 +1,35 @@
 package com.senac.navegacaotelas.screens
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
+
+
+@Composable
+fun Profile(){
+
+    Column (
+        modifier = Modifier
+            .fillMaxSize()
+    ){
+        Text(
+            text = "Perfil",
+            textAlign = TextAlign.Center,
+            fontSize = 24.sp,
+            modifier = Modifier
+                .fillMaxSize()
+        )
+    }
+}
+
+
+@Preview(showBackground = true)
+@Composable
+fun PreviewProfile(){
+    Profile()
+}
